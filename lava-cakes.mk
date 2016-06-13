b8ccdded205941945d7bce699535551cf0dbed58
3on <jr@ailleurs.me>
2016-06-13 00:50:45 -0700

    Add lava cakes

---
+++ lava-cakes.mk
@@ -0,0 +1,15 @@
+**Ingredients**
+
+- 100g of 70% chocolate
+- 120g of sugar
+- 90g of butter
+- 3 eggs
+- 40g flour
+- a fuckload of amaretto
+
+**Steps**
+
+1. Melt chocolate and butter
+2. Mix eggs and sugar
+3. Mix 1 and 2
+4. Add the flour
