1bf1bc6ee630bf4904de35136fd2517e9fd51b55
Jr Prévost <jr@3on.fr>
2017-02-14 18:48:38 -0800

    Create reine-de-saba.mk

---
+++ reine-de-saba.mk
@@ -0,0 +1,17 @@
+#Reine de Saba
+
+##Ingredients
+* 200g sugar
+* 200g butter
+* 200g chocolate
+* 6 eggs
+* 100g flour
+* 10cc of strong coffee
+
+
+1. Mix Yolks and sugar until they whiten
+2. Melt butter and chocolate, then add coffee
+3. Beat the crap of the egg white until you have a solid white foam
+4. Mix 1 and 2 together, **add flour**
+5. Add 3 into 4 genly, by hand not with a whip, need to keep the whites texture
+6. Cook for 15 to 20min at 180c
